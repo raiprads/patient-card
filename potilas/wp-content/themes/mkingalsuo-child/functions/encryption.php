875b09beea3b4dba0bba7355bddc244b0abe6b42
raiprads <raiprads@gmail.com>
2017-02-02 20:05:16 +0200

    Add keys for extra protection in the encryption process.

--- potilas/wp-content/themes/mkingalsuo-child/functions/encryption.php
+++ potilas/wp-content/themes/mkingalsuo-child/functions/encryption.php
@@ -12,6 +12,9 @@ function encrypt_data($string, $password) {
 
 	$encyption_type = "AES-128-CBC";
 
+	$key = get_your_global_keys();
+	$password = $password . $key;
+
 	return openssl_encrypt($string, $encyption_type, $password, 0 , "");
 
 }
@@ -20,7 +23,22 @@ function decrypt_data($string, $password) {
 
 	$encyption_type = "AES-128-CBC";
 
+	$key = get_your_global_keys();
+	$password = $password . $key;
+
 	return openssl_decrypt($string, $encyption_type, $password, 0 , "");
+}
+
+function get_your_global_keys() {
+
+	// remember to update this key before running the whole system for security purposes
+    $filename = __DIR__ . '/../key/key.txt';
+    $fp = @fopen($filename, 'r'); 
+
+    if ($fp) {
+    	return fread($fp, filesize($filename));
+	}
+
 }
 
 ?>
